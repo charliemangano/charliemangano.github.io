b952d515e01e8f1ac308c01493956a4b98c2391c
charlie mangano <eva.mangano@epfl.ch>
2025-01-16 18:12:12 +0100

    feat: add custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'mono': "Disket\\ Mono"
+      }
+    },
   },
   plugins: [],
 }
