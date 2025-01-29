25b7df0d109788300d2bee98155db125cc82b839
charlie mangano <eva.mangano@epfl.ch>
2025-01-29 10:10:02 +0100

    fix: fix font not applied to the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        mono: ["Disket Mono", ...defaultTheme.fontFamily.mono, 'monospace'],
+        mono: ['var(--font-mono)', 'monospace'],
       },
     },
   },
